8a1f2172faf66ad838922d60ad358e87638d295d
heywhyyou <havenoreasontolive@gmail.com>
2023-09-09 19:57:17 +0300

    feat: add swiper placeholder

---
+++ script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  effect: "cards",
+  grabCursor: true,
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev"
+  // }
+});
