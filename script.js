9133721bbb4660ac2fc7b3003750ea1c5a4f72b0
heywhyyou <havenoreasontolive@gmail.com>
2023-09-17 02:51:28 +0300

    feat: finish footer and swiper

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+const stars = Array.from(document.querySelectorAll(".feedback__star"));
+
 const swiper = new Swiper(".mySwiper", {
   direction: "vertical",
   effect: "cards",
   grabCursor: true,
+  keyboard: true,
+  loop: true,
+  mousewheel: true,
+  cardsEffect: {
+    slideShadows: false,
+    rotate: false,
+    perSlideOffset: 10,
+  },
   // navigation: {
   //   nextEl: ".swiper-button-next",
   //   prevEl: ".swiper-button-prev"
   // }
+});
+
+document.querySelector(".stars").addEventListener("click", function (e) {
+  console.log(e.target);
+});
+
+let userRating = 0;
+
+stars.forEach((item) => {
+  item.addEventListener("click", () => {
+    const { itemValue } = item.dataset;
+    item.parentNode.dataset.totalValue = itemValue;
+    userRating = itemValue;
+  });
 });
