216ed42d8b07334b41284e281b249b33b04f4f79
heywhyyou <havenoreasontolive@gmail.com>
2023-09-21 00:56:43 +0300

    feat: make header, hero & footer responsive

--- script.js
+++ script.js
@@ -12,29 +12,4 @@ const swiper = new Swiper(".mySwiper", {
     rotate: false,
     perSlideOffset: 10,
   },
-  // navigation: {
-  //   nextEl: ".swiper-button-next",
-  //   prevEl: ".swiper-button-prev"
-  // }
-});
-
-/*
- <!-- <div class="swiper-controls">
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
-              </div> -->
-*/
-
-document.querySelector(".stars").addEventListener("click", function (e) {
-  console.log(e.target);
-});
-
-let userRating = 0;
-
-stars.forEach((item) => {
-  item.addEventListener("click", () => {
-    const { itemValue } = item.dataset;
-    item.parentNode.dataset.totalValue = itemValue;
-    userRating = itemValue;
-  });
 });
