f2ac9488c6eb97b9e0fc3b06d53c1b410685393e
heywhyyou <havenoreasontolive@gmail.com>
2023-09-28 19:30:15 +0300

    feat: add swiper background, replace icons, some refactoring

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dog Product Website
+
+This is a repository for my Dog Product website. It showcases various dog products and provides information for dog owners.
+
+## Deploy
+
+You can find the deploy link [here](https://dog-product-by-hey.netlify.app/).
+
+## Figma Design
+
+Please find the Figma design for this project [here](https://www.figma.com/file/gL9JS8pG9ioQBoOzn34aSv/This?type=design&node-id=9882%3A41&mode=dev).
+
+## Technologies Used
+
+- SCSS: Used for styling the website, allowing for efficient and modular CSS development.
+- BEM: Followed BEM (Block Element Modifier) methodology for clear and structured CSS class naming.
+- Swiper: Utilized Swiper library for creating interactive and responsive slider.
--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+// 📫 Open Modal
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+btnsOpenModal.forEach((item) => item.addEventListener("click", openModal));
+
+// 📪 Close Modal Actions
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// Close modal, button
+btnCloseModal.addEventListener("click", closeModal);
+
+// Close modal, overlay
+overlay.addEventListener("click", closeModal);
+
+// Close modal, ESC
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
+
 const swiper = new Swiper(".mySwiper", {
   direction: "vertical",
   effect: "cards",
@@ -11,32 +41,3 @@ const swiper = new Swiper(".mySwiper", {
     perSlideOffset: 10,
   },
 });
-
-/*
-
-// 📫 Open Modal
-const openModal = function () {
-	modal.classList.remove("hidden");
-	overlay.classList.remove("hidden");
-};
-
-btnsOpenModal.forEach(item => item.addEventListener("click", openModal));
-
-// 📪 Close Modal Actions
-const closeModal = function () {
-	modal.classList.add("hidden");
-	overlay.classList.add("hidden");
-};
-
-// Close modal, button
-btnCloseModal.addEventListener("click", closeModal);
-
-// Close modal, overlay
-overlay.addEventListener("click", closeModal);
-
-// Close modal, ESC
-document.addEventListener("keydown", function (e) {
-	if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
-});
-
-*/
