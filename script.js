03755b0eb6941ce5b65fbb357964b04a0fb3910a
heywhyyou <havenoreasontolive@gmail.com>
2023-09-28 05:02:28 +0300

    feat: fix slider stars, add header links, add media mixins

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const stars = Array.from(document.querySelectorAll(".feedback__star"));
-
 const swiper = new Swiper(".mySwiper", {
   direction: "vertical",
   effect: "cards",
@@ -13,3 +11,32 @@ const swiper = new Swiper(".mySwiper", {
     perSlideOffset: 10,
   },
 });
+
+/*
+
+// 📫 Open Modal
+const openModal = function () {
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
+};
+
+btnsOpenModal.forEach(item => item.addEventListener("click", openModal));
+
+// 📪 Close Modal Actions
+const closeModal = function () {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+};
+
+// Close modal, button
+btnCloseModal.addEventListener("click", closeModal);
+
+// Close modal, overlay
+overlay.addEventListener("click", closeModal);
+
+// Close modal, ESC
+document.addEventListener("keydown", function (e) {
+	if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
+
+*/
